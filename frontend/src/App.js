476e31a9c1ff714a5151706a668bb307c0fd330d
SinghAniket24 <aniket.singh.st1@gmail.com>
2025-11-17 20:18:12 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="banner">
+      <h1>Welcome to Certinexa</h1>
+      <p>Blockchain-Based Digital Certificate System</p>
+    </div>
+  );
+}
+
+export default App;
