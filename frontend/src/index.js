562aa4125b226ddec60a42cd6e55b6d4168f67a7
DallisRodrigues <rodriguesdallis@gmail.com>
2025-11-20 10:52:16 +0530

    auth and dashboards first draft

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,16 @@ import './index.css';
 // import App from './App';
 
 //  Current page you want to view
-//import Register from './organization/register';
+import Register from './organization/register';
 //import RecipientAuth from './recepient/Auth/RecepientAuth';
 //import AdminDashboard from './admin/pages/AdminDashboard';
-import AdminAuth from './admin/pages/AdminAuth';
+//import AdminAuth from './admin/pages/AdminAuth';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
     {/* Replace this component anytime to view other pages */}
-    <AdminAuth/>
+    <Register/>
 
     {/* <App />  // ← uncomment this to go back to App.js */}
 
